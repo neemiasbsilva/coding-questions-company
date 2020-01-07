72881892db8f269c56137db2fa5b61389579a684
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-06 22:33:10 -0400

    Count the triplest where the sum is equal to the third element

--- arrays/count-the-triplets.cpp
+++ arrays/count-the-triplets.cpp
@@ -6,8 +6,38 @@ Thanks :)
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
+
+void count_triplest(vector<int> array, int N){
+    int max_val = 0;
+
+    for(int i = 0; i < N; i++)
+        max_val = max(max_val, array[i]);
+    
+    int frequence[max_val + 1];
+    for(int i = 0; i < max_val+1; i++){
+        frequence[i] = 0;
+    }
+
+    for(int i = 0; i < N; i++){
+        frequence[array[i]]++;
+    }
+
+    int ans = 0;
+    //Case 1: (0, 0, 0)
+    ans += frequence[0] * frequence[0]-1 * frequence[0]-2 / 6;
+
+    //Case 2: (0, x, x)
+    for(int i = 1; i <= max_val; i++){
+        ans += frequence[0] * frequence[i] * (frequence[i] -1) / 2;
+    }
+
+    //Case 3: ()
+    for()
+
+}
 
 int main(){
     int T; //number of test case
@@ -21,23 +51,7 @@ int main(){
             cin >> array[i];
         }
         
-        for(int i = 0; i < N; i++){
-            sum += array[i];
-            cont = 1;
-            for(int j = i+1; j < N; j++){
-                if(array[j] > array[j-1])
-                    sum+= array[i];
-                    cont++;
-                
-                if(cont == 3){
-                    if(sum == array[j])
-                        triplets++;
-                    sum = array[i];
-                }
-            }
-        }
-        if(triplets > 0) cout << triplets << endl;
-        else cout << "-1" << endl;
+        count_triplest(array, N)
     }
 
     return 0;
