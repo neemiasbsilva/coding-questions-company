88e59470fa1e87789bc810d36bd0cf1686b28e65
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-06 21:39:56 -0400

    Count the triplest where the sum is equal to the third element

---
+++ arrays/count-the-triplets.cpp
@@ -0,0 +1,19 @@
+/*
+For check the question, please copy this link in your
+browser: https://practice.geeksforgeeks.org/problems/count-the-triplets/0
+Thanks :)
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int T; //number of test case
+    cin >> T;
+    while(T--){
+
+    }
+
+    return 0;
+}
