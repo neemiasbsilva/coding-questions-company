f9fe37543907fb9623aa8da6bd444bd91404ea28
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-07 10:24:54 -0400

    Find subarray with given sum using the method simple

---
+++ arrays/subarray-sum-nonnegative-simple.py
@@ -0,0 +1,36 @@
+# Method 1 Simple
+import numpy as np
+
+def subArraySum(arr, n, s):
+
+    for i in range(n):
+        current_sum = arr[i]
+        j = i + 1
+
+        while j <= n:
+            if current_sum == s:
+                print("%d %d"% (i+1, j))
+                return 1
+            
+            if current_sum > s or j == n:
+                break
+            
+            current_sum += arr[j]
+            j += 1
+    
+    return 0
+def main():
+    T = int(input())
+
+    for i in range(T):
+        N, S = list(map(int,input().split(' ')))
+        N = int(N)
+        S = int(S)
+        # arr = np.zeros(shape=(N), dtype=np.int32)
+        arr = list(map(int,input().split(' ')))
+        arr = np.asarray(arr)
+        if subArraySum(arr, N, S) is 0:
+            print("-1")
+
+if __name__ == "__main__":
+    main()
