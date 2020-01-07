4aae9d02ff4cc8865f5b10742d7efc411391c3bd
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-06 22:55:28 -0400

    Count the triplest where the sum is equal to the third element

---
+++ arrays/count-the-triplest.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def count_the_triplest(array, N):
+    # construct the hash
+    max_value = array.max()
+
+    answer = 0
+    frequence = np.zeros(shape=max_value+1)
+    for i in array:
+        frequence[i] += 1
+
+    # Case 1: (0, 0, 0)
+    answer += frequence[0] * (frequence[0]-1) * (frequence[0]-2) / 6
+
+    # Case 2: (0, x, x)
+    for i in range(1, max_value):
+        answer += frequence[0] * frequence[i] * (frequence[i]-1) / 2
+
+    # Case 3: (x, x, 2x)
+    for i in range(1, (max_value+1) // 2):
+        answer += (frequence[i] *
+                    (frequence[i]-1) // 2 * frequence[2*i])
+
+    # Case 4: (x, y, x+y)
+    for i in range(1, max_value+1):
+        for j in range(i+1, max_value-i+1):
+            answer += frequence[i] * frequence[j] * frequence[i+j]
+
+    return answer
+
+def main():
+    T = input()
+    for i in range(0, T):
+        N = input()
+        array = np.zeros(shape=(N))
+
+        array[i] = input()
+
+        answer = count_the_triplest(array, N)
+
+if __name__ == "__main__":
+    main()
