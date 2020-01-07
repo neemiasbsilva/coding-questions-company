5bc10ab24a5c77f5fd351785b6e3bae7367429e9
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-06 22:40:07 -0400

    Count the triplest where the sum is equal to the third element

--- arrays/count-the-triplets.cpp
+++ arrays/count-the-triplets.cpp
@@ -10,7 +10,7 @@ Thanks :)
 
 using namespace std;
 
-void count_triplest(vector<int> array, int N){
+int count_triplest(vector<int> array, int N){
     int max_val = 0;
 
     for(int i = 0; i < N; i++)
@@ -34,9 +34,18 @@ void count_triplest(vector<int> array, int N){
         ans += frequence[0] * frequence[i] * (frequence[i] -1) / 2;
     }
 
-    //Case 3: ()
-    for()
+    //Case 3: (x, x, 2*x)
+    for(int i = 1; 2*i <= max_val; i++){
+        ans += frequence[i] * (frequence[i] -1) / 2 * (frequence[2*i]);
+    }
 
+    //Case 4: (x, y, x+y)
+    for(int i = 1; i <= max_val; i++){
+        for(int j = i+1; i+j <= max_val; j++)
+         ans += frequence[i] * frequence[j] * frequence[i+j];
+    }
+
+    return ans;
 }
 
 int main(){
@@ -51,7 +60,10 @@ int main(){
             cin >> array[i];
         }
         
-        count_triplest(array, N)
+        int ans = count_triplest(array, N);
+
+        if(ans > 0) cout << ans << endl;
+        else cout << "-1" << endl;
     }
 
     return 0;
