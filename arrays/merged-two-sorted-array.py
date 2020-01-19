513ea7b2fe8c2d0bbd66534d864b0c98d4831f8a
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-19 14:48:29 -0400

    Merge without extra space

--- arrays/merged-two-sorted-array.py
+++ arrays/merged-two-sorted-array.py
@@ -9,7 +9,10 @@ def main():
         size_arr1, size_arr2 = raw_input().split(' ')
         size_arr1 = int(size_arr1)
         size_arr2 = int(size_arr2)
-        print(size_arr1, size_arr2)
+        arr1 = list(map(int, raw_input().split(' ')))
+        arr2 = list(map(int, raw_input().split(' ')))
+        print(arr1)
+        print(arr2)
 
 
 if __name__ == "__main__":
