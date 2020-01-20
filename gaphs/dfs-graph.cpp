c2b9375031a4ab179b1f9d1563df25c20673f298
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-20 19:00:05 -0400

    Implementing DFS of graph

--- gaphs/dfs-graph.cpp
+++ gaphs/dfs-graph.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+
+#define D(x) cout << #x << " = " << (x) << endl
+#define endl '\n'
 
 using namespace std;
 
@@ -8,14 +12,45 @@ typedef vector<int> vi;
 
 vi dfs_num;
 
+class Graph{
+    public:
+        int V;
+        vi *AdjList;
+        Graph(int V);
+        void addEdge(int u, int v);
+        void dfs(int u);
+};
 
-void addEdge(vii AdjList, int u, int v){
+Graph::Graph(int V){
+    this->V = V;
+    AdjList = new vi[V];
+}
+
+
+void Graph::addEdge(int u, int v){
     AdjList[u].push_back(v);
     AdjList[v].push_back(u);
 }
 
 
-int main(){
+void Graph::dfs(int u){
+    dfs_num
+}
 
+int main(){
+    int n_test;
+    cin >> n_test;
+
+    while(n_test--){
+
+        int V, E;
+        cin >> V >> E;
+        Graph g(V);
+        for(int i = 0; i < V; i++){
+            int u, v;
+            cin >> u >> v;
+            g.addEdge(u, v);
+        }
+    }
     return 0;
 }
