ee015ed4d1d6155ef690e36e2ea152a69347ded9
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-20 18:44:26 -0400

    Implementing DFS of graph

--- gaphs/dfs-graph.cpp
+++ gaphs/dfs-graph.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-class Graph()
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+vi dfs_num;
+
+
+void addEdge(vii AdjList, int u, int v){
+    AdjList[u].push_back(v);
+    AdjList[v].push_back(u);
+}
 
 
 int main(){
