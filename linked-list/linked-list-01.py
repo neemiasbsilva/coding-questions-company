b337242ded964ff014c34308a4fb6eeb7b0c2953
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-25 17:59:01 -0300

    Implementing the Sorted LinkedList (first problem)

--- linked-list/linked-list-01.py
+++ linked-list/linked-list-01.py
@@ -1,5 +1,9 @@
 '''
 	Given a sorted linked list and a value to insert, write a function to insert the value in a sorted way.
+	Input: 2 => 5 => 7 => 10 => 15
+	Insert the value 9.
+	Output: 2 => 5 => 7 => 9 => 10 => 15
+
 '''
 
 # Create a single node to linked list
@@ -9,13 +13,19 @@ class Node:
 		self.data = data
 		self.next = next
 
-#
+
+#Create a LinkedList Class to join all nodes
+class LinkedList:
+
+	def __init__(self):
+		self.head = None
 
 
 
 
 def main():
-	L = list()
+	ll = LinkedList()
+	ll.head = Node()
 
 
 
