5f131c4c326719f2f9137909bc8fd8735f651258
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-25 17:52:29 -0300

    Solving the problem01 of linked list

---
+++ linked-list/linked-list-01.py
@@ -0,0 +1,23 @@
+'''
+	Given a sorted linked list and a value to insert, write a function to insert the value in a sorted way.
+'''
+
+# Create a single node to linked list
+class Node:
+	"""constructor"""
+	def __init__(self, data, next=None):
+		self.data = data
+		self.next = next
+
+#
+
+
+
+
+def main():
+	L = list()
+
+
+
+if __name__ == '__main__':
+	main()
