9a3c62a57203aa59bbf655925ac20bb5df386db6
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-27 15:07:37 -0300

    Implementing the reverse list

---
+++ linked-list/linked-list-reverse.py
@@ -0,0 +1,37 @@
+'''
+	Given a linked list of N nodes. The task is to reverse the list
+''' 
+
+
+class Node:
+	# constructor
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class LinkedList:
+
+	# constructor
+	def __init__(self):
+		self.head = None
+
+
+	# insertion
+
+
+
+
+def main():
+	n_cases = int(input())
+
+	for i in n_cases:
+		ll = LinkedList()
+		length = int(input())
+		arr_input = list(map(int, input().split(' ')))
+
+		
+
+
+
+if __name__ == '__main__':
+	main()
