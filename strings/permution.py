f6c1f5ae103a609216145a569659d644989d051f
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-30 16:59:31 -0300

    Permutations of string

--- strings/permution.py
+++ strings/permution.py
@@ -1,13 +1,33 @@
 
 
+def permutation(s, l, r):
+
+	# Base case
+
+	if l == r:
+		print(s)
+	else:
+
+		# Permutation made
+
+		for i in range(l, r):
+			s[l], s[r] = s[i], s[l]
+
+			#recursion called
+			permutation(l+1, r)
+
+			#backtrack
+			s[l], s[r] = s[i], s[l]
+
+
+
 def main():
 	n_case = int(input())
 
 	for i in range(n_case):
 		s = str(input())
 
-		for j in range(len(s)):
-			print(s[j])
-
+		permutation(s, 0, len(s))
+		
 if __name__ == '__main__':
 	main()
