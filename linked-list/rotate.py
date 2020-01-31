2a760c4221a4d82103a475285c04a840567a2b6d
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-31 12:13:46 -0300

    Implementing rotate linked list

--- linked-list/rotate.py
+++ linked-list/rotate.py
@@ -1,4 +1,10 @@
 
+class Node:
+
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
 
 main():
 
@@ -9,6 +15,8 @@ main():
 		length = int(input())
 
 		arr = list(map(int, input().split(' ')))
+
+
 
 if __name__ == '__main__':
 	main()
