7c0625d980b0e6ec507b29edd954411e9d3d382d
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-31 14:59:10 -0300

    Snapchat problem coding interview

---
+++ arrays/snapchat-problem.py
@@ -0,0 +1,20 @@
+'''
+	Given an array of time intervals (start, end) for classroom lectures 
+	(possibly overlapping), find the minimum number of rooms required.
+'''
+import numpy as np
+
+def main():
+	n_case = int(input())
+
+	for i in range(n_case):
+
+		length = int(input())
+
+		l = list(map(int, input().split(' ')))
+
+
+
+
+if __name__ == '__main__':
+	main()
