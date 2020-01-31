e65cb95418b7358ef1b3c4211d2ae365340311fb
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-31 12:40:55 -0300

    Implementing rotate linked list

--- linked-list/rotate.py
+++ linked-list/rotate.py
@@ -33,22 +33,22 @@ class LinkedList:
 
 		current = self.head
 		count  = 1
-		while count < k and current:
+		while count < k and current is not None:
 			curent = current.next
 			count += 1
 
-		
+		if current is None:
+			return
+
 		kthNode = current
 
 		while current.next is not None:
 			current = current.next
 
 
-
-
 		current.next = self.head
 
-		self.head = kthNode
+		self.head = kthNode.next
 
 		kthNode = None
 
