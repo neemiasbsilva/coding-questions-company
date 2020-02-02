47cf4643671f95d24053007fa8f75c3da31eea95
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-02 17:09:22 -0300

    Longest Palindrome in a String (Incomplety)

--- strings/logest-palindrome.py
+++ strings/logest-palindrome.py
@@ -3,7 +3,31 @@ def longest_palindrome(s):
 
     dp = [[0 for x in range(len(s))] for y in range(len(s))]
 
-    print(dp)
+    length = 1
+    i = 0
+
+    while i < n:
+        dp[i][i] = True
+        i += 1
+    
+    start = 0
+    i = 0
+    while i < len(s)-1:
+        if s[i] == s[i+1]:
+            dp[i][i+1] = True
+            start = i
+            length= 2
+        i += 1
+    
+
+    k = 3
+    while k <= len(s):
+        i = 0
+
+        while i < (len(s)-k+1):
+            j = i + k - 1
+
+        
 
 def main():
     n_case = int(input())
