355f6c23732e49ab4290186c4ff85922a9f31c42
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-03 14:20:27 -0400

    Longest Palindrome in a String

--- strings/longest-palindrome.py
+++ strings/longest-palindrome.py
@@ -1,3 +1,10 @@
+import sys
+
+
+def print_string(s, start, n):
+    sys.stdout.write(s[start:n+1])
+    sys.stdout.flush()
+    return ''
 
 def longest_palindrome(s):
 
@@ -6,7 +13,7 @@ def longest_palindrome(s):
     length = 1
     i = 0
 
-    while i < n:
+    while i < len(s):
         dp[i][i] = True
         i += 1
     
@@ -26,6 +33,17 @@ def longest_palindrome(s):
 
         while i < (len(s)-k+1):
             j = i + k - 1
+
+            if dp[i+1][j-1] and s[i] == s[j]:
+                dp[i][j] = True
+            
+                if k > length:
+                    length = k
+                    start = i
+            
+            i += 1
+        k += 1
+    print(print_string(s, start, start+length-1))
 
         
 
