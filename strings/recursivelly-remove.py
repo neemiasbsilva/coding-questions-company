126e31d3edc2b584ce3a086c94164b64be4df55e
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-04 11:38:55 -0400

    Recursivelly remove all adjacent duplicate (implementing)

--- strings/recursivelly-remove.py
+++ strings/recursivelly-remove.py
@@ -3,13 +3,25 @@
     The output string should not have any adjacent duplicates.
 '''
 
+def removeUtil(s, last_removed):
+    
+
+def to_string(s):
+    return ''.join(s)
+
+def remove(s):
+    last_removed = 0
+    return to_string(removeUtil(s, last_removed))
+
 def main():
     n_case = int(input())
 
     for i in range(n_case):
         s = str(input())
 
-        
+        s = list(s)
+
+        remove(s)
 
 if __name__ == "__main__":
     main()
