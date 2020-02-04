5fccf4681a4d82fad510b8b792c8a34c175f0d5f
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-04 11:20:50 -0400

    Recursivelly remove all adjacent duplicate (implementing)

---
+++ strings/recursivelly-remove.py
@@ -0,0 +1,13 @@
+'''
+    Given a string s, recursively remove adjacent duplicate character from the strings.
+    The output string should not have any adjacent duplicates.
+'''
+
+def main():
+    n_case = int(input())
+
+    for i in range(n_case):
+        
+
+if __name__ == "__main__":
+    main()
