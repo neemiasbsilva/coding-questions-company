d2788372037a7b6b17223608da2e886ade1f6862
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-06 11:56:30 -0400

    Next Large Element of the array using stack

--- stack-and-queue/next-large-element.py
+++ stack-and-queue/next-large-element.py
@@ -1,4 +1,32 @@
-import numpy as np
+def next_large_element(arr):
+    element = 0
+    next = 0
+    stack = []
+
+    stack.append(arr[0])
+
+    for i in range(1, len(arr), 1):
+        next = arr[i]
+        if len(stack) is not 0:
+            
+            element = stack.pop()
+
+            while element < next:
+                # print(str(element)+' -- '+str(next))
+                print(str(next), end=' ')
+                if len(stack) == 0:
+                    break
+                element = stack.pop()
+
+            if element > next:
+                stack.append(element)
+        stack.append(next)
+
+    while len(stack) is not 0:
+        element = stack.pop()
+        next = -1
+        # print(str(element) + " -- " + str(next))
+        print(str(next), end=' ')
 
 
 def main():
@@ -9,12 +37,8 @@ def main():
         n = int(input())
         arr = list(map(int, input().split(' ')))
 
-        result = np.zeros(shape=(n))
-        for j in range(len(arr)-1):
-            
-            print(max(arr[j+1:]))
-        
-        
+        next_large_element(arr)
+        print('')
 
 if __name__ == "__main__":
     main()
